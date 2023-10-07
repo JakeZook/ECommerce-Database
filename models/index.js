b1d36e1341d15e05fc20189ec1a8fa2ac8a6dc0b
Jake Zook <jakezook01@gmail.com>
2023-10-06 22:33:54 -0600

    Updated README.

--- models/index.js
+++ models/index.js
@@ -8,12 +8,11 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, { foreignKey: "category_id" });
 // Categories have many Products
 Category.hasMany(Product);
-// Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
 	foreignKey: "tag_id",
 	through: ProductTag,
 });
-// // // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
 	foreignKey: "product_id",
 	through: ProductTag,
